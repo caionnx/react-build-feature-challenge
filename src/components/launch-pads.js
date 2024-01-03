3b121c81a5c37fb9bd7b47a196ccf0d4fefe9b3c
Caio Nunes <caionavarro.n@hotmail.com>
2024-01-03 16:23:39 +0000

    feat(launch-pads): add favorite functionality

--- src/components/launch-pads.js
+++ src/components/launch-pads.js
@@ -1,10 +1,12 @@
-import { Badge, Box, SimpleGrid, Text } from "@chakra-ui/react";
+import { Badge, Box, SimpleGrid, Text, Flex } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
 import Error from "./error";
 import Breadcrumbs from "./breadcrumbs";
 import LoadMoreButton from "./load-more-button";
 import { useSpaceXPaginatedQuery } from "../utils/use-space-x";
+import FavoriteButton from "./favorite-button";
+import { FAVORITES_TYPES } from "../contexts/FavoritesContext";
 
 const PAGE_SIZE = 12;
 
@@ -57,38 +59,47 @@ function LaunchPadItem({ launchPad }) {
       position="relative"
     >
       <Box p="6">
-        <Box d="flex" alignItems="baseline">
-          {launchPad.status === "active" ? (
-            <Badge px="2" variant="solid" colorScheme="green">
-              Active
-            </Badge>
-          ) : (
-            <Badge px="2" variant="solid" colorScheme="red">
-              Retired
-            </Badge>
-          )}
-          <Box
-            color="gray.500"
-            fontWeight="semibold"
-            letterSpacing="wide"
-            fontSize="xs"
-            textTransform="uppercase"
-            ml="2"
-          >
-            {launchPad.launch_attempts} attempted &bull;{" "}
-            {launchPad.launch_successes} succeeded
-          </Box>
-        </Box>
+        <Flex gap="6" alignItems="center" justifyContent="space-between">
+          <Box minW="0">
+            <Box d="flex" alignItems="baseline">
+              {launchPad.status === "active" ? (
+                <Badge px="2" variant="solid" colorScheme="green">
+                  Active
+                </Badge>
+              ) : (
+                <Badge px="2" variant="solid" colorScheme="red">
+                  Retired
+                </Badge>
+              )}
+              <Box
+                color="gray.500"
+                fontWeight="semibold"
+                letterSpacing="wide"
+                fontSize="xs"
+                textTransform="uppercase"
+                ml="2"
+                isTruncated
+              >
+                {launchPad.launch_attempts} attempted &bull;{" "}
+                {launchPad.launch_successes} succeeded
+              </Box>
+            </Box>
 
-        <Box
-          mt="1"
-          fontWeight="semibold"
-          as="h4"
-          lineHeight="tight"
-          isTruncated
-        >
-          {launchPad.full_name}
-        </Box>
+            <Box
+              mt="1"
+              fontWeight="semibold"
+              as="h4"
+              lineHeight="tight"
+              isTruncated
+            >
+              {launchPad.full_name}
+            </Box>
+          </Box>
+          <Box>
+            <FavoriteButton type={FAVORITES_TYPES.PADS} id={launchPad.id} />
+          </Box>
+        </Flex>
+        
         <Text color="gray.500" fontSize="sm">
           {launchPad.rockets.map((r) => r.name).join(", ")}
         </Text>
